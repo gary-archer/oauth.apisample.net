6375505903ee075fd94025b836971896c63e7956
gary-archer <garyarcher36@gmail.com>
2026-09-22 19:23:45 +0100

    Updates

--- integration_tests.sh
+++ integration_tests.sh
@@ -12,7 +12,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 dotnet build
 
 #
-# On Linux ensure that the API has permissions to listen on a port below 1024
+# On Linux ensure that tests have permissions to listen on a port below 1024
 #
 if [ "$(uname -s)" == 'Linux' ]; then
   sudo setcap 'cap_net_bind_service=+ep' ./test/bin/Debug/net10.0/test
